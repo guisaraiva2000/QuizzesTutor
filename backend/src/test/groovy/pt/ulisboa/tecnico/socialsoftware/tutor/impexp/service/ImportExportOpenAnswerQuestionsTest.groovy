f22e66a51762f06cc70a5352ca9b6dadaaa769c5
guisaraiva2000 <guilherme.a.saraiva@tecnico.ulisboa.pt>
2021-03-22 10:17:44 +0000

    #close 28, 33, 36

---
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/tutor/impexp/service/ImportExportOpenAnswerQuestionsTest.groovy
@@ -0,0 +1,69 @@
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.context.TestConfiguration
+import pt.ulisboa.tecnico.socialsoftware.tutor.BeanConfiguration
+import pt.ulisboa.tecnico.socialsoftware.tutor.SpockTest
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.domain.Question
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.ImageDto
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.OpenAnswerQuestionDto
+import pt.ulisboa.tecnico.socialsoftware.tutor.question.dto.QuestionDto
+
+@DataJpaTest
+class ImportExportOpenQuestionsTest extends SpockTest {
+
+    def questionId
+
+    def setup() {
+        def questionDto = new QuestionDto()
+
+        questionDto.setTitle(QUESTION_1_TITLE)
+        questionDto.setContent(QUESTION_1_CONTENT)
+        questionDto.setStatus(Question.Status.AVAILABLE.name())
+
+        def openAnswerQuestionDto = new OpenAnswerQuestionDto()
+
+
+        def image = new ImageDto()
+        image.setUrl(IMAGE_1_URL)
+        image.setWidth(20)
+        questionDto.setImage(image)
+
+        // TODO
+
+        questionDto.setQuestionDetailsDto(openAnswerQuestionDto)
+
+        questionId = questionService.createQuestion(externalCourse.getId(), questionDto).getId()
+    }
+
+    def 'export and import questions to xml'() {
+        given: 'a xml with questions'
+        def questionsXml = questionService.exportQuestionsToXml()
+        print questionsXml
+        and: 'a clean database'
+        questionService.removeQuestion(questionId)
+
+        when:
+        questionService.importQuestionsFromXml(questionsXml)
+
+        then:
+        questionRepository.findQuestions(externalCourse.getId()).size() == 1
+        def questionResult = questionService.findQuestions(externalCourse.getId()).get(0)
+        questionResult.getKey() == null
+        questionResult.getTitle() == QUESTION_1_TITLE
+        questionResult.getContent() == QUESTION_1_CONTENT
+        questionResult.getStatus() == Question.Status.AVAILABLE.name()
+        def imageResult = questionResult.getImage()
+        imageResult.getWidth() == 20
+        imageResult.getUrl() == IMAGE_1_URL
+
+
+        def openAnswerDetailsDto = (OpenAnswerQuestionDto) questionResult.getQuestionDetailsDto()
+        // TODO
+    }
+
+    def 'export to latex'() {
+        // TODO
+    }
+
+    @TestConfiguration
+    static class LocalBeanConfiguration extends BeanConfiguration {}
+}
